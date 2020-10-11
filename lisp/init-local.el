18ecbd532fcbb0b7ce465177f67ceddb8df0bce9
morrislchg <402114096@qq.com>
2020-10-11 11:08:32 +0800

    娣诲姞鏈湴閰嶇疆鏂囦欢

---
+++ lisp/init-local.el
@@ -0,0 +1,6 @@
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                           ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(custom-set-variables
+ '(markdown-command "C:/Users/Administrator/AppData/Local/Pandoc/pandoc.exe"))
+(set-language-environment "Chinese-GB") 
+(provide 'init-local)
---
+++ lisp/init-preload-local.el
@@ -0,0 +1,3 @@
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                           ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(provide 'init-preload-local)
